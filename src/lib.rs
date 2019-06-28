0cf860abd46ac256c13524cee49011f6c0b4e368
hjiayz <hjiayz@163.com>
2019-06-28 11:43:24 +0800

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -26,24 +26,26 @@ pub fn map<T, U, F: FnOnce(T) -> U>(bound: Bound<T>, f: F) -> Bound<U> {
         Bound::Unbounded => Bound::Unbounded,
     }
 }
-pub fn as_ref<U, T: AsRef<U>>(bound: &Bound<T>) -> Bound<&U> {
+pub fn as_ref<U: ?Sized, T: AsRef<U>>(bound: &Bound<T>) -> Bound<&U> {
     match bound {
         Bound::Excluded(b) => Bound::Excluded(b.as_ref()),
         Bound::Included(b) => Bound::Included(b.as_ref()),
         Bound::Unbounded => Bound::Unbounded,
     }
 }
-pub fn as_mut<U, T: AsMut<U>>(bound: &mut Bound<T>) -> Bound<&mut U> {
+pub fn as_mut<U: ?Sized, T: AsMut<U>>(bound: &mut Bound<T>) -> Bound<&mut U> {
     match bound {
         Bound::Excluded(b) => Bound::Excluded(b.as_mut()),
         Bound::Included(b) => Bound::Included(b.as_mut()),
         Bound::Unbounded => Bound::Unbounded,
     }
 }
-pub fn as_pin_ref<'a, U, T: AsRef<U>>(bound: Pin<&'a Bound<T>>) -> Bound<Pin<&'a U>> {
+pub fn as_pin_ref<'a, U: ?Sized, T: AsRef<U>>(bound: Pin<&'a Bound<T>>) -> Bound<Pin<&'a U>> {
     unsafe { map(as_ref(Pin::get_ref(bound)), |x| Pin::new_unchecked(x)) }
 }
-pub fn as_pin_mut<'a, U, T: AsMut<U>>(bound: Pin<&'a mut Bound<T>>) -> Bound<Pin<&'a mut U>> {
+pub fn as_pin_mut<'a, U: ?Sized, T: AsMut<U>>(
+    bound: Pin<&'a mut Bound<T>>,
+) -> Bound<Pin<&'a mut U>> {
     unsafe {
         map(as_mut(Pin::get_unchecked_mut(bound)), |x| {
             Pin::new_unchecked(x)
@@ -107,25 +109,25 @@ impl<T> MyBound<T> {
     pub fn map<U, F: FnOnce(T) -> U>(self, f: F) -> MyBound<U> {
         MyBound(map(self.0, f))
     }
-    pub fn as_ref<U>(&self) -> MyBound<&U>
+    pub fn as_ref<U: ?Sized>(&self) -> MyBound<&U>
     where
         T: AsRef<U>,
     {
         MyBound(as_ref(&self.0))
     }
-    pub fn as_mut<U>(&mut self) -> MyBound<&mut U>
+    pub fn as_mut<U: ?Sized>(&mut self) -> MyBound<&mut U>
     where
         T: AsMut<U>,
     {
         MyBound(as_mut(&mut self.0))
     }
-    pub fn as_pin_ref<'a, U>(self: Pin<&'a MyBound<T>>) -> MyBound<Pin<&'a U>>
+    pub fn as_pin_ref<'a, U: ?Sized>(self: Pin<&'a MyBound<T>>) -> MyBound<Pin<&'a U>>
     where
         T: AsRef<U>,
     {
         MyBound(unsafe { map(as_ref(&Pin::get_ref(self).0), |x| Pin::new_unchecked(x)) })
     }
-    pub fn as_pin_mut<'a, U>(self: Pin<&'a mut MyBound<T>>) -> MyBound<Pin<&'a mut U>>
+    pub fn as_pin_mut<'a, U: ?Sized>(self: Pin<&'a mut MyBound<T>>) -> MyBound<Pin<&'a mut U>>
     where
         T: AsMut<U>,
     {
@@ -191,13 +193,20 @@ pub trait MyRangeBounds<T: ?Sized>: RangeBounds<T> {
 impl<T, U: RangeBounds<T>> MyRangeBounds<T> for U {}
 
 impl<T> Into<Bound<T>> for MyBound<T> {
-    fn into(self)->Bound<T> {
+    fn into(self) -> Bound<T> {
         self.0
     }
 }
 
 impl<T> From<Bound<T>> for MyBound<T> {
-    fn from(src:Bound<T>)->MyBound<T> {
+    fn from(src: Bound<T>) -> MyBound<T> {
         MyBound(src)
     }
+}
+
+#[test]
+fn test_as_ref() {
+    let s: MyBound<String> = Bound::Included("abcd".to_owned()).into();
+    let u: &[u8] = s.as_ref().unwrap();
+    assert_eq!(u, b"abcd");
 }
